ca1319cd1cba11d61b25b8614163d42934e61f9b
HadHod <maciej.mlynarski@gmail.com>
2021-12-31 10:33:54 +0100

    some new functions

--- main.ts
+++ main.ts
@@ -13,10 +13,74 @@ export function getNumberOfDigits(n: number): number {
   return Math.floor(Math.log10(Math.abs(n))) + 1;
 }
 
-export function isNumber(n: any): boolean {
-  return !isNaN(parseFloat(n)) && isFinite(n);
+export function isNumber(a: any): boolean {
+  return !isNaN(parseFloat(a)) && isFinite(a);
 }
 
-export function isArray(o: any): boolean {
-  return Object.prototype.toString.call(o) === '[object Array]';
+export function isArray(a: any): boolean {
+  return Object.prototype.toString.call(a) === '[object Array]';
 };
+
+export function getUnique(array: any[]): any[] {
+  return [...new Set(array)];
+}
+
+// uniqueBy(
+//   (a: any, b: any) => a.id == b.id,
+//     [
+//       { id: 0, value: "a" },
+//       { id: 1, value: "b" },
+//       { id: 2, value: "c" },
+//       { id: 1, value: "d" },
+//       { id: 0, value: "e" },
+//     ],
+//   );
+
+// [ { id: 0, value: 'a' }, { id: 1, value: 'b' }, { id: 2, value: 'c' } ]
+
+export function uniqueBy(fn: Function, array: any[]) {
+  array.reduce((acc, v) => {
+    if (!acc.some((x: any) => fn(v, x))) {
+      acc.push(v);
+    }
+    return acc;
+  }, []);
+}
+
+export function sum(...arr: number[]): number {
+  return [...arr].reduce((acc, val) => acc + val, 0);
+}
+
+export function getRandomInt(min = 0, max = 100): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function isEmpty(val: any) {
+  return val == null || !(Object.keys(val) || val).length;
+}
+
+// const users = [
+//   { name: "fred", age: 48 },
+//   { name: "barney", age: 36 },
+//   { name: "fred", age: 40 },
+// ];
+// orderBy(users, ["name", "age"], ["asc", "desc"]); // [{name: 'barney', age: 36}, {name: 'fred', age: 48}, {name: 'fred', age: 40}]
+// orderBy(users, ["name", "age"]); // [{name: 'barney', age: 36}, {name: 'fred', age: 40}, {name: 'fred', age: 48}]
+
+const orderBy = <T>(
+  arr: T[],
+  props: (keyof T)[],
+  orders?: ("asc" | "desc")[]
+) =>
+  [...arr].sort((a, b) =>
+    props.reduce((acc, prop, i) => {
+      if (acc === 0) {
+        const [p1, p2] =
+          orders && orders[i] === "desc"
+            ? [b[prop], a[prop]]
+            : [a[prop], b[prop]];
+        acc = p1 > p2 ? 1 : p1 < p2 ? -1 : 0;
+      }
+      return acc;
+    }, 0)
+  );
